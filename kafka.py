56adaf54e8033bff71782390df51dc29bff0c1c1
Max Murakami <mmurakam@redhat.com>
2021-07-05 16:58:05 +0200

    fixed kafka consumer subscription

--- kafka.py
+++ kafka.py
@@ -23,16 +23,16 @@ def kafka_consumer(bootstrap_service_url, group_id='my-group',
             'group.id': group_id,
         }
     )
-    consumer._subscribe([topic])
+    consumer.subscribe([topic])
     return consumer
 
 
-def publish_to_bridge(bridge_service_url, *messages, topic='my-topic'):
+def publish_to_bridge(bridge_service_name, *messages, port=8080, topic='my-topic'):
     payload = _to_bridge_payload(*messages)
-    topic_url = _to_topic_url(bridge_service_url, topic)
+    topic_url = _to_topic_url(bridge_service_name, port, topic)
     headers = {'content-type': 'application/vnd.kafka.json.v2+json'}
     
-    log.info(f'Publishing message to topic "{topic}" at {bridge_service_url}.')
+    log.info(f'Publishing message to topic "{topic}" at {topic_url}.')
     log.debug(f'Payload: {pformat(payload)}')
     
     response = requests.post(topic_url, json=payload, headers=headers)
@@ -49,8 +49,8 @@ def _to_bridge_payload(*items):
     return payload
 
 
-def _to_topic_url(bridge_service_url, topic):
-    return f'{bridge_service_url}/topics/{topic}'
+def _to_topic_url(bridge_service_name, port, topic):
+    return f'http://{bridge_service_name}:{port}/topics/{topic}'
 
 
 class KafkaHttpClient:
